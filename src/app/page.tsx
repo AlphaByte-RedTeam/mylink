62ea6b00e33854db7d4e1951d11dc95d140ca51f
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-03-04 15:55:11 +0700

    Add footer section (#2)
    * chore(deps): install tailwindcss
    
    * feat: init tailwind & postcss config
    
    * chore: configure tailwind template paths
    
    * feat: add tailwind directives to global css
    
    * asset: add gradient for background
    
    * feat: add vscode typescript workspace
    
    * chore: change metadata title and desc
    
    * feat: use inter family from next font
    
    * chore: extend tailwind color and dropshadow
    
    * asset: add my photo
    
    * chore(deps): add iconoir react
    
    * style: set background color of body to black
    
    * chore: add icons to metadata
    
    * feat: add page content
    
    * style: add hover effect to button
    
    * chore(deps): install tailwind scrollbar hide
    
    * chore: add tailwind scrollbar hide plugin
    
    * feat: add skillset and role
    
    * chore: add target to link
    
    * chore: add font fallback
    
    * style: adjust layout
    
    * asset: change vercel logo color
    
    * feat: add deployed by vercel
    
    * chore: wrap with footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,6 +139,16 @@ export default function Home() {
           python
         </p>
       </div>
+      <footer>
+        <Link href="https://vercel.com" target="_blank">
+          <div className="flex flex-row justify-center z-20 items-center gap-2">
+            <p className="text-white text-base font-semibold font-sans">
+              Deployed by {""}
+            </p>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </div>
+        </Link>
+      </footer>
     </main>
   )
 }
