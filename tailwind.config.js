5fcce9b60446ac6cea12f6a0df52c5e3bf78ad3d
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-03-04 13:41:11 +0700

    chore: extend tailwind color and dropshadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        glassmorphishm: {
+          8: 'rgba(32, 32, 32, 0.08)',
+          38: 'rgba(32, 32, 32, 0.38)',
+        },
+      },
+      dropShadow: {
+        'glassmorphism': '0 10px 20px rgba(0, 0, 0, 0.05)',
+      },
+
+    },
   },
   plugins: [],
 }
